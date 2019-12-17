016507f1594d26dd263af7aff238fc708c1610fe
Alvin Tan <alvintan@Alvins-MacBook-Air.local>
2019-12-17 05:00:32 -0800

    test added for text area validation

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import CommentBox from 'components/CommentBox'
+import {mount} from 'enzyme'
+
+let wrapped;
+
+beforeEach(()=>{
+    wrapped = mount(<CommentBox/>)
+})
+afterEach(()=>{
+    wrapped.unmount()
+})
+
+it('has a text area and a button',()=>{
+    expect(wrapped.find('textarea').length).toEqual(1)
+    expect(wrapped.find('button').length).toEqual(1)
+})
+
+describe('the text area', ()=>{
+    beforeEach(()=>{
+        wrapped.find('textarea').simulate('change',{
+            target: {value: 'new comment'}
+        })
+        wrapped.update() //rerenders the react code after set state
+    })
+    
+    it('has a text area that users can type in',()=>{
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+    })
+    
+    it('has empties the text area after user submits form',()=>{
+        wrapped.find('form').simulate('submit')
+        wrapped.update()
+        expect(wrapped.find('textarea').prop('value')).toEqual('')
+    })
+})
