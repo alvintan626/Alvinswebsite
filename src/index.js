48ca4ac664843e56a62efe97ed16410821568814
Alvin Tan <alvintan@Alvins-MacBook-Air.local>
2019-12-17 19:37:40 -0800

    integrated redux to app component, error in tests bc not integrated in tests

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from 'components/App'
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+//Redux setup
+import {Provider} from 'react-redux' //provider is the redux store
+import { createStore } from 'redux' // connect function to cnenct to redux store
+import reducers from 'reducers'
+
+ReactDOM.render(
+    <Provider store={createStore(reducers,{})}>
+        <App />
+    </Provider>
+, document.querySelector('#root'))
