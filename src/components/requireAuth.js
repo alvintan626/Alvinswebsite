5ad3202c8b7da7122b557c6198b799ba73b670c1
Alvin Tan <alvintan@Alvins-Air.gateway.sonic.net>
2019-12-23 19:55:45 -0800

    created HOC file and added scaffold, then move the reusable logic in

---
+++ src/components/requireAuth.js
@@ -0,0 +1,33 @@
+// this file naming convention (lowercase first letter)
+// means this file will be used for exporting functions rather than class based usage
+
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+export default (ChildComponent) => {
+    class ComposedComponent extends Component {
+        componentDidMount(){
+            this.shouldNavigateAway()
+        }
+    
+        //whenever commentbox recieve new props
+        componentDidUpdate(){
+            this.shouldNavigateAway()
+        }
+    
+        shouldNavigateAway(){
+            if (!this.props.auth) {
+                this.props.history.push('/')
+            }
+        }
+
+        render(){
+            return <ChildComponent/>
+        }
+    }
+
+    function mapStateToProps(state){
+        return {auth: state.auth}
+    }
+
+    return connect(mapStateToProps)(ComposedComponent)
+}
